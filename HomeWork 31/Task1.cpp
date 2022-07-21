9f2bee947fda401b5d2fb1bb827fc4b308216032
maksimbutakov59 <maksimbutakov59@gmail.com>
2022-07-22 00:00:37 +0500

    .

--- HomeWork 31/Task1.cpp
+++ HomeWork 31/Task1.cpp
@@ -5,8 +5,10 @@
 
 int task1(int n, int m) {
 	srand(time(NULL));
-	if (n >= m)
+	if (n > m)
 		throw std::invalid_argument("Начало диапозона больше конца");
+	if (n == m)
+		throw std::invalid_argument("Начало равно концу диапозона.");
 	int k = rand() % (m - n) + n;
 	std::cout << "Случайное число из диапозона -> ";
 	return k;
